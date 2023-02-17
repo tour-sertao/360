312c39cf488ec24647d990097fcf8eb35299a668
tour-sertao <125682873+tour-sertao@users.noreply.github.com>
2023-02-17 14:59:51 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,72 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_fly_20221031_162358_35_1667244619678_pano_optimized",
+      "name": "dji_fly_20221031_162358_35_1667244619678_pano_optimized",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.9598938796635679,
+        "pitch": 0.24422980918034298,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.671226478034331,
+          "pitch": 0.35533113921224313,
+          "rotation": 0,
+          "target": "1-dji_fly_20221031_165522_46_1667246636993_pano_optimized"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dji_fly_20221031_165522_46_1667246636993_pano_optimized",
+      "name": "dji_fly_20221031_165522_46_1667246636993_pano_optimized",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -3.0869373941689737,
+        "pitch": 0.5288902023028221,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
